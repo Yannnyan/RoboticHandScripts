282c74f78e58d58e327d696a18b0618dca766b9e
Guy Gur-Arieh <72988190+TumbleHood@users.noreply.github.com>
2023-08-20 15:57:45 +0300

    Add files via upload

--- get_finger_point/get_finger_point.py
+++ get_finger_point/get_finger_point.py
@@ -26,13 +26,21 @@ def Rz(a):
     ])
 
 def R(a, b, c):
-    return Rz(math.radians(a)) @ Ry(math.radians(b)) @ Rx(math.radians(c))
+    return np.round(Rz(math.radians(a)) @ Ry(math.radians(b)) @ Rx(math.radians(c)), decimals=10)
 
 finger_to_distances = {"thumb":  [0.025718, 0.025138, 0.032887, 0.034182],
                        "index":  [0.100255, 0.038364, 0.024583, 0       ],
                        "middle": [0.096798, 0.043421, 0.027867, 0       ],
                        "ring":   [0.091677, 0.039445, 0.026879, 0       ],
-                       "pinky":  [0.042660, 0.046176, 0.031074, 0.020545]}
+                       "pinky":  [0.042660, 0.046176, 0.031074, 0.020545],
+                       "test3":  [1, 1, 1, 0],
+                       "test4":  [1, 1, 1, 1]}
+
+finger_to_inits = {"thumb":  [-134.20247233590717, 78.45535380354426, -8.248710073793161],
+                   "index":  [161.6191695699214, 60.56787240129933, 8.913981287221988],
+                   "middle": [170.1049499379885, 48.48982791314379, 6.539613189147546],
+                   "ring":   [-178.93383120555032, 38.87611924302098, -0.829998668591034],
+                   "pinky":  [-160.3063691606456, 20.67568305317016, -18.378102639715692]}
 
 def get_finger_point(wrist_rotation, joint_rotations, finger):
 
@@ -46,14 +54,14 @@ def get_finger_point(wrist_rotation, joint_rotations, finger):
     R1 = R(a1, b1, c1)
     R2 = R(a2, b2, c2)
 
-    p1_relative = np.array([l1, 0, 0]).T
+    p1_relative = np.array([l1, 0, 0])
     p1_effect = R0 @ p1_relative
 
-    p2_relative = np.array([l2, 0, 0]).T
-    p2_effect = R0 @ R1 @ p2_relative
+    p2_relative = np.array([l2, 0, 0])
+    p2_effect = R1 @ p2_relative
 
-    p3_relative = np.array([l3, 0, 0]).T
-    p3_effect = R0 @ R1 @ R2 @ p3_relative
+    p3_relative = np.array([l3, 0, 0])
+    p3_effect = R2 @ p3_relative
 
     if l4 == 0:
         p3 = p1_effect + p2_effect + p3_effect
@@ -62,8 +70,8 @@ def get_finger_point(wrist_rotation, joint_rotations, finger):
     a3, b3, c3 = joint_rotations[2]
     R3 = R(a3, b3, c3)
 
-    p4_relative = np.array([l4, 0, 0]).T
-    p4_effect = R0 @ R1 @ R2 @ R3 @ p4_relative
+    p4_relative = np.array([l4, 0, 0])
+    p4_effect = R3 @ p4_relative
     
     p4 = p1_effect + p2_effect + p3_effect + p4_effect
 
@@ -73,24 +81,27 @@ def get_finger_points(wrist_rotation, joint_rotations, finger):
 
     points = []
 
-    a0, b0, c0 = wrist_rotation
+    a0init, b0init, c0init = finger_to_inits[finger]
+    a0wrist, b0wrist, c0wrist = wrist_rotation
     a1, b1, c1 = joint_rotations[0]
     a2, b2, c2 = joint_rotations[1]
 
     l1, l2, l3, l4 = finger_to_distances[finger]
 
-    R0 = R(a0, b0, c0)
+    Rinit = R(a0init, b0init, c0init)
+    Rwrist = R(-a0wrist, -b0wrist, -c0wrist)
+    R0 = Rwrist @ Rinit
     R1 = R(a1, b1, c1)
     R2 = R(a2, b2, c2)
 
-    p1_relative = np.array([l1, 0, 0]).T
+    p1_relative = np.array([l1, 0, 0])
     p1_effect = R0 @ p1_relative
 
-    p2_relative = np.array([l2, 0, 0]).T
-    p2_effect = R0 @ R1 @ p2_relative
+    p2_relative = np.array([l2, 0, 0])
+    p2_effect = R1 @ p2_relative
 
-    p3_relative = np.array([l3, 0, 0]).T
-    p3_effect = R0 @ R1 @ R2 @ p3_relative
+    p3_relative = np.array([l3, 0, 0])
+    p3_effect = R2 @ p3_relative
 
     p1 = p1_effect
     p2 = p1 + p2_effect
@@ -106,8 +117,8 @@ def get_finger_points(wrist_rotation, joint_rotations, finger):
     a3, b3, c3 = joint_rotations[2]
     R3 = R(a3, b3, c3)
 
-    p4_relative = np.array([l4, 0, 0]).T
-    p4_effect = R0 @ R1 @ R2 @ R3 @ p4_relative
+    p4_relative = np.array([l4, 0, 0])
+    p4_effect = R3 @ p4_relative
     
     p4 = p3 + p4_effect
     points.append(p4)
--- get_finger_point/test.py
+++ get_finger_point/test.py
@@ -1,20 +1,23 @@
 from get_finger_point import get_finger_points
+from calculate_rotation_matrix import *
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
-def draw_hand(w, t, i, m, r, p):
+def draw_hand(t, i, m, r, p):
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
     for f in [t, i, m, r, p]:
-        ax.plot([w[0], f[0][0]], [w[1], f[0][1]], zs=[w[2], f[0][2]])
+        ax.plot([0, f[0][0]], [0, f[0][1]], [0, f[0][2]])
         for j in range(len(f)-1):
-            ax.plot([f[j][0], f[j+1][0]], [f[j][1], f[j+1][1]], zs=[f[j][2], f[j+1][2]])
+            ax.plot([f[j][0], f[j+1][0]], [f[j][1], f[j+1][1]], [f[j][2], f[j+1][2]])
 
-    # for p in [w + t + i + m + r + p]:
-    #     ax.scatter(p[0], p[1], p[2], marker='o')
+    for f in [[[0, 0, 0,]], t, i, m, r, p]:
+        for p in f:
+            ax.scatter(p[0], p[1], p[2], marker='o')
 
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
@@ -22,29 +25,93 @@ def draw_hand(w, t, i, m, r, p):
 
     plt.show()
 
+def vec_abs(vec):
+    return math.sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
+
 def main():
     df = pd.read_csv("PositionBonesRight.csv")
-    a = df.to_numpy()[0][1:-1]
+    a = df.to_numpy()[3][1:-1]
     positions = np.array_split(a[:54], 18)
     rotations = np.array_split(a[54:], 18)
 
-    w = positions[-1]
-    t = positions[0:4]
-    i = positions[4:7]
-    m = positions[7:10]
-    r = positions[10:13]
-    p = positions[13:17]
+    everything = np.array_split(a, 18+18)
+    wrist_position = everything[17]
 
-    draw_hand(w, t, i, m, r, p)
+    wrist = rotations[0]
+    thumb = rotations[1:5]
+    index = rotations[5:8]
+    middle = rotations[8:11]
+    ring = rotations[11:14]
+    pinky = rotations[14:18]
 
-    w = [0, 0, 0]
-    t = get_finger_points(rotations[0], rotations[1:5], "thumb")
-    i = get_finger_points(rotations[0], rotations[5:8], "index")
-    m = get_finger_points(rotations[0], rotations[8:11], "middle")
-    r = get_finger_points(rotations[0], rotations[11:14], "ring")
-    p = get_finger_points(rotations[0], rotations[14:18], "pinky")
+    tp = positions[0:4]
+    ip = positions[4:7]
+    mp = positions[7:10]
+    rp = positions[10:13]
+    pp = positions[13:17]
 
-    draw_hand(w, t, i, m, r, p)
+    # print(calculate_angles(tp[0], tp[1]))
+    # print(thumb[0])
+
+    # t_init = np.round([math.degrees(math.asin(tp[0][1]/vec_abs(tp[0]))), math.degrees(math.asin(tp[0][0]/vec_abs(tp[0]))), math.degrees(math.asin(tp[0][2]/vec_abs(tp[0])))], decimals=10)
+    # i_init = np.round([math.degrees(math.asin(ip[0][1]/vec_abs(ip[0]))), math.degrees(math.asin(ip[0][0]/vec_abs(ip[0]))), math.degrees(math.asin(ip[0][2]/vec_abs(ip[0])))], decimals=10)
+    # m_init = np.round([math.degrees(math.asin(mp[0][1]/vec_abs(mp[0]))), math.degrees(math.asin(mp[0][0]/vec_abs(mp[0]))), math.degrees(math.asin(mp[0][2]/vec_abs(mp[0])))], decimals=10)
+    # r_init = np.round([math.degrees(math.asin(rp[0][1]/vec_abs(rp[0]))), math.degrees(math.asin(rp[0][0]/vec_abs(rp[0]))), math.degrees(math.asin(rp[0][2]/vec_abs(rp[0])))], decimals=10)
+    # p_init = np.round([math.degrees(math.asin(pp[0][1]/vec_abs(pp[0]))), math.degrees(math.asin(pp[0][0]/vec_abs(pp[0]))), math.degrees(math.asin(pp[0][2]/vec_abs(pp[0])))], decimals=10)
+
+    # for i in [t_init, i_init, m_init, r_init, p_init, "\n"]:
+    #     print(i)
+
+    Rwrist = R(wrist[0], wrist[1], wrist[2])
+    t_wrist = Rwrist @ np.array(tp[0])
+    i_wrist = Rwrist @ np.array(ip[0])
+    m_wrist = Rwrist @ np.array(mp[0])
+    r_wrist = Rwrist @ np.array(rp[0])
+    p_wrist = Rwrist @ np.array(pp[0])
+
+    t_init = calculate_init(t_wrist)
+    i_init = calculate_init(i_wrist)
+    m_init = calculate_init(m_wrist)
+    r_init = calculate_init(r_wrist)
+    p_init = calculate_init(p_wrist)
+
+    for i in [t_init, i_init, m_init, r_init, p_init, "\n"]:
+        print(i)
+
+    draw_hand(tp, ip, mp, rp, pp)
+
+    tr = get_finger_points(wrist, thumb, "thumb")
+    ir = get_finger_points(wrist, index, "index")
+    mr = get_finger_points(wrist, middle, "middle")
+    rr = get_finger_points(wrist, ring, "ring")
+    pr = get_finger_points(wrist, pinky, "pinky")
+
+    draw_hand(tr, ir, mr, rr, pr)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    for f in [tp, ip, mp, rp, pp, tr, ir, mr, rr, pr]:
+        ax.plot([0, f[0][0]], [0, f[0][1]], [0, f[0][2]])
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+    # for ps in [[tp, tr], [ip, ir], [mp, mr], [rp, rr], [pp, pr]]:
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(projection='3d')
+    #     for p in ps:
+    #         ax.plot([0, p[0][0]], [0, p[0][1]], zs=[0, p[0][2]])
+    #         for j in range(len(p)-1):
+    #             ax.plot([p[j][0], p[j+1][0]], [p[j][1], p[j+1][1]], zs=[p[j][2], p[j+1][2]])
+    #     # for j in range(len(ps[0])-1):
+    #     #     print(math.dist(ps[0][j], ps[0][j+1]), end=", ")
+    #     #     print(math.dist(ps[0][j], ps[0][j+1]))
+    #     plt.show()
+
 
 
 if __name__ == "__main__":
